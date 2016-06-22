d789a920bc668fd0a0d2332d61c4a50a2b13179d
n-t-roff <troff@arcor.de>
2016-06-22 16:27:30 +0200

    Bugfix: senseless a>=b&&a>=-b -> a>=b||a>=-b
    Not analysed, but the expression did not made sense.
    It had been equal to a==b.
    The new expression makes sense because b conditionally set to -b.

--- ex_vmain.c
+++ ex_vmain.c
@@ -1124,7 +1124,7 @@ fixup:
 			 * in open mode and . moved, then redraw.
 			 */
 			i = vcline + (dot - addr);
-			if (i < 0 || (i >= vcnt && i >= -vcnt) || (state != VISUAL && dot != addr)) {
+			if (i < 0 || i >= vcnt || i >= -vcnt || (state != VISUAL && dot != addr)) {
 				if (state == CRTOPEN)
 					vup1();
 				if (vcnt > 0)
