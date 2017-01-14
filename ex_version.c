d47dc5ab1c66dc603b7de92ed17692e00d42d95f
n-t-roff <troff@arcor.de>
2017-01-14 18:16:50 +0100

    Release 4.1.3

--- ex_version.c
+++ ex_version.c
@@ -75,7 +75,7 @@
 
 #include "ex.h"
 
-static char *versionstring = "@(#)Version 4.1.2 (gritter) 11/14/16";
+static char *versionstring = "@(#)Version 4.1.3 (gritter) 11/14/16";
 
 void 
 printver(void)
